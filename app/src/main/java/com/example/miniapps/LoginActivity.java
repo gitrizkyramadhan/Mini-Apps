4366fb8f8f33328c3ab98d38f373291bd71138ea
rizky.ramadhan <rama.yuhu@gmail.com>
2020-02-09 22:22:29 +0700

    create login page

---
+++ app/src/main/java/com/example/miniapps/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.example.miniapps;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final EditText emailEditText = findViewById(R.id.etEmail);
+        final EditText passEditText = findViewById(R.id.etPass);
+        final Button loginBtn = findViewById(R.id.btnLogin);
+        
+    }
+}
